975a08efcbc431441fddacc3378991c80444df6e
GarlicScent <dongzai@naver.com>
2022-11-26 00:42:10 +0900

    부분합 재귀적으로 푸는 코드 연습

--- jsWorkspace/etc/array.js
+++ jsWorkspace/etc/array.js
@@ -1,41 +1,19 @@
 //부분합 문제. 아래는 처음에 알지 못했을 때 이렇게 진행했다.
 let arr = [1, 2, 5, 1, 0];
 //여기서 합이 7인 경우를 찾기.
+let found = [];
 
-for (let i = 0; i < arr.length; i++) {
-	for (let j = i + 1; j < arr.length; j++) {
-		if (arr[i] + arr[j] === 7) {
-			console.log("2중 포문에서");
-			console.log(i, j, arr[i], arr[j]);
-			count++;
-		}
-		for (let k = j + 1; k < arr.length; k++) {
-			if (arr[i] + arr[j] + arr[k] === 7) {
-				console.log("3중 포문에서");
-				console.log(i, j, k, arr[i], arr[j], arr[k]);
-				count++;
-			}
-			for (let h = k + 1; h < arr.length; h++) {
-				if (arr[i] + arr[j] + arr[k] + arr[h] === 7) {
-					console.log("4중 포문에서");
-					console.log(i, j, k, h, arr[i], arr[j], arr[k], arr[h]);
-					count++;
-				}
-			}
-		}
+const funcSum = (i, target, array) => {
+	if (i === 0) {
+		if (target === 0) return true;
+		else return false;
 	}
-}
 
-/*
-let count = 0;
-let sum = 0;
-for (let i = 0; i < arr.length; i++) {
-	sum = arr[i];
-	for (let j = i + 1; j < arr.length; j++) {
-		sum += arr[j];
-		if (sum === 7) count++;
-	}
-}
-*/
+	if (funcSum(i - 1, target, array)) return true;
 
-console.log(count);
+	if (funcSum(i - 1, target - array[i - 1], array)) return true;
+
+	return false;
+};
+
+console.log(funcSum(5, 7, arr));
