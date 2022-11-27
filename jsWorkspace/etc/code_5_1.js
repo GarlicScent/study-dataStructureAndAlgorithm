4bc570a9600a622acdf8eeea4797e4f4c54434e7
GarlicScent <dongzai@naver.com>
2022-11-27 19:31:27 +0900

    개구리 뛰는 문제 풀이 fix

--- jsWorkspace/etc/code_5_1.js
+++ jsWorkspace/etc/code_5_1.js
@@ -1,19 +1,17 @@
 let h = [2, 9, 4, 5, 1, 6, 10];
 let dp = h.map((x) => -1);
-let N = h.length;
 dp[0] = 0;
 console.log(dp, h);
 
-for (let i = 1; i < N; i++) {
+for (let i = 1; i < h.length; i++) {
 	if (i == 1) {
 		dp[i] = Math.abs(h[i] - h[i - 1]);
-		console.log(Math.abs(h[i] - h[i - 1]));
 	} else {
 		dp[i] = Math.min(
-			Math.abs(dp[i - 1] + h[i] - h[i - 1]),
-			Math.abs(dp[i - 2] + h[i] - h[i - 2])
+			dp[i - 1] + Math.abs(h[i] - h[i - 1]),
+			dp[i - 2] + Math.abs(h[i] - h[i - 2])
 		);
 	}
 }
-console.log(`dp[${N - 1}]: `, dp[N - 1]);
+console.log(`dp[${h.length - 1}]: `, dp[h.length - 1]);
 console.log(dp);
