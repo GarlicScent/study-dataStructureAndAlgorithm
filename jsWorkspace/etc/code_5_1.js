4bfa2fc63748fd800e09dd5e1f116e0f41701cdb
GarlicScent <dongzai@naver.com>
2022-11-28 23:14:49 +0900

    for문을 forEach로 바꾸기

--- jsWorkspace/etc/code_5_1.js
+++ jsWorkspace/etc/code_5_1.js
@@ -5,16 +5,28 @@ let dp = h.map((x) => INF);
 dp[0] = 0;
 console.log(dp, h);
 
-for (let i = 1; i < h.length; i++) {
-	if (i == 1) {
-		dp[i] = Math.abs(h[i] - h[i - 1]);
+// for (let i = 1; i < h.length; i++) {
+// 	if (i == 1) {
+// 		dp[i] = Math.abs(h[i] - h[i - 1]);
+// 	} else {
+// 		dp[i] = Math.min(
+// 			dp[i - 1] + Math.abs(h[i] - h[i - 1]),
+// 			dp[i - 2] + Math.abs(h[i] - h[i - 2])
+// 		);
+// 	}
+// }
+
+dp.forEach((val, i, arr) => {
+	if (i === 0) {
+	} else if (i === 1) {
+		arr[i] = Math.abs(h[i] - h[i - 1]);
 	} else {
-		dp[i] = Math.min(
-			dp[i - 1] + Math.abs(h[i] - h[i - 1]),
-			dp[i - 2] + Math.abs(h[i] - h[i - 2])
+		arr[i] = Math.min(
+			arr[i - 1] + Math.abs(h[i] - h[i - 1]),
+			arr[i - 2] + Math.abs(h[i] - h[i - 2])
 		);
 	}
-}
+});
 
 console.log(`dp[${h.length - 1}]: `, dp[h.length - 1]);
 console.log(dp);
