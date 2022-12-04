926508caf551c36cba80cf2ed3cefaa7a969c34a
GarlicScent <dongzai@naver.com>
2022-12-04 14:15:23 +0900

    5.7 구간 분할 최적화 예제 코드

---
+++ cWorkspace/chapterFive/code_5_9.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <class T>
+void chmin(T &a, T b)
+{
+  if (a > b)
+  {
+    a = b;
+  }
+}
+
+const long long INF = 1LL << 60;
+
+int main()
+{
+  //입력
+  int N;
+  cin >> N;
+  vector<vector<long long>> c(N + 1, vector<long long>(N + 1));
+
+  for (int i = 0; i < N + 1; ++i)
+  {
+    for (int j = 0; j < N + 1; ++j)
+    {
+      cin >> c[i][j];
+    }
+  }
+  // DP 테이블 정의
+  vector<long long> dp(N + 1, INF);
+  // DP 초기 조건
+  dp[0] = 0;
+  // DP 반복
+  for (int i = 0; i <= N; ++i)
+  {
+    for (int j = 0; j < i; ++j)
+    {
+      chmin(dp[i], dp[j] + c[j][i]);
+    }
+  }
+
+  //답 출력
+  cout << dp[N] << endl;
+}
